8b80a71c2f01c696b37889faa4d4050114f9ce71
Augustine59-Wangombe <wangombeaugustine58@gmail.com>
2025-12-17 10:55:19 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,31 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAeKFby7DVFy80702igVrqN-dauNiK-C_Q",
+  authDomain: "nyeri-catholic-youth-app.firebaseapp.com",
+  databaseURL: "https://nyeri-catholic-youth-app-default-rtdb.firebaseio.com",
+  projectId: "nyeri-catholic-youth-app",
+  storageBucket: "nyeri-catholic-youth-app.firebasestorage.app",
+  messagingSenderId: "2807748399",
+  appId: "1:2807748399:web:a33abb5ea33a2d387bb3da",
+  measurementId: "G-9HRL1S4BDP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 import {
   getAuth,
   createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAeKby7DVfy8070ZjgVRqN-dauNiK_CrQ",
-  authDomain: "nyeri-catholic-youth-app.firebaseapp.com",
-  projectId: "nyeri-catholic-youth-app",
-  storageBucket: "nyeri-catholic-youth-app.appspot.com",
-  messagingSenderId: "2807748399",
-  appId: "1:2807748399:web:a33bb5ea33a2ad87bb3da",
-  measurementId: "G-9HRL154BDP",
-};
-
-const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
@@ -63,4 +73,5 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
 
